e5146cc4e2c2478ca72503847d020514f1c0b3eb
Jonathan Ray <jonathanray17@gmail.com>
2019-05-01 14:11:59 -0400

    adds routing between the pages

---
+++ src/All.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Weather from './Weather'
+import Joke from './Joke'
+import GOT from './GOT'
+import Fortune from './Fortune'
+function All(props){
+return(<>
+<Weather />
+<Joke />
+<GOT />
+<Fortune />
+</>)
+}
+export default All;
--- src/App.js
+++ src/App.js
@@ -4,17 +4,31 @@ import Weather from './Weather';
 import Fortune from './Fortune';
 import Joke from './Joke';
 import GOT from './GOT';
+import All from './All';
+import {Link, Route, Switch, withRouter} from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <div style={{backgroundColor: `#282c34`}} className="App">
-      <h2 style={{margin: `0`, color: `#FFF`} }>React Dashboard</h2>
+    <div style={{backgroundColor: `#282c34`, color: `#FFF`}} className="App">
+      <h2 style={{margin: `0`} }>React Dashboard</h2>
+      <Link style={{color: `#FFF`}} to="/all" >All</Link><br />
+      <Link style={{color: `#FFF`}} to="/weather" >Weather</Link><br />
+      <Link style={{color: `#FFF`}} to="/fortune" >Fortune</Link><br />
+      <Link style={{color: `#FFF`}} to="/joke" >Joke</Link><br />
+      <Link style={{color: `#FFF`}} to="/got" >GOT</Link><br />
       <header className="App-header">
-      <Weather />
-      <Fortune />
-      <Joke />
-      <GOT />
+      <Route path="/" exact component={All} />
+      <Route path="/all" component={All} />
+      <Route path="/weather" component={Weather} />
+      <Route path="/fortune" component={Fortune} />
+      <Route path="/joke" component={Joke} />
+      <Route path="/got" component={GOT} />
+      {/* <Weather /> */}
+      {/* <Fortune /> */}
+      {/* <Joke /> */}
+      {/* <GOT /> */}
       </header>
     </div>
   );
